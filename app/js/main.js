2fa024f06ea6994e5cb1fa0cb966b50c2117e17e
temastepan <temastepan@gmail.com>
2020-02-15 18:55:27 +0500

    added items page

--- app/js/main.js
+++ app/js/main.js
@@ -4,8 +4,7 @@ $(function(){
   $(".rate-star").rateYo({
     rating: 5,
     starWidth: "12px",
-    readOnly: true
-    
+    readOnly: true    
   });
 
   $('.product-slider__inner').slick({
@@ -13,9 +12,28 @@ $(function(){
     arrows: false,
     slidesToShow: 4,
     slidesToScroll: 4
-
   });
 
+  $(".js-range-slider").ionRangeSlider({
+    type: "double",
+    min: 0,
+    max: 1000,
+    from: 0,
+    to: 600,
+    grid: true,
+    prefix: "$"
+});
+
+$('.icon-th-list').on('click', function(){
+  $('.product__item').addClass('list');
+  $('.icon-th-list').addClass('active');
+  $('.icon-th-large').removeClass('active');
+});
+$('.icon-th-large').on('click', function(){
+  $('.product__item').removeClass('list');
+  $('.icon-th-large').addClass('active');
+  $('.icon-th-list').removeClass('active');
+});
 
   var mixer = mixitup('.products__inner-box');
 
